8f10939e2fe5cd2aff17b2d8b3d1cf1a71be7c82
Jonathan B Whitaker <jbwhitak@lenny.eng.utah.edu>
2015-03-21 17:19:05 -0600

    Added C bouncer

---
+++ bouncer.c
@@ -0,0 +1,104 @@
+#include <stdio.h>
+#include <string.h>
+#include "libavutil/frame.h"
+#include "libavutil/pixfmt.h"
+#include "libavformat/avformat.h"
+#include "libavcodec/avcodec.h"
+
+AVFrame* OpenImage(const char* imageFileName)
+{
+  AVFormatContext *pFormatCtx;
+
+  if(av_open_input_file(&pFormatCtx, imageFileName, NULL, 0, NULL) != 0)
+    {
+      printf("Can't open image file '%s'\n", imageFileName);
+      return NULL;
+    }       
+
+  dump_format(pFormatCtx, 0, imageFileName, 0);
+
+  AVCodecContext *pCodecCtx;
+
+  pCodecCtx = pFormatCtx->streams[0]->codec;
+  pCodecCtx->pix_fmt = AV_PIX_FMT_RGB24;
+
+  // find the decoder for the stream
+  AVCodec *pCodec = avcodec_find_decoder(pCodecCtx->codec_id);
+  if (!pCodec)
+    {
+      printf("Codec not found\n");
+      return NULL;
+    }
+
+  // open codec
+  if(avcodec_open(pCodecCtx, pCodec)<0)
+    {
+      printf("Could not open codec\n");
+      return NULL;
+    }
+
+  AVFrame *pFrame;
+
+  pFrame = avcodec_alloc_frame();
+  if (!pFrame)
+    {
+      printf("Can't allocate memory for AVFrame\n");
+      return NULL;
+    }
+
+  int frameFinished;
+  int numBytes;
+
+  // determine required buffer size and allocate buffer
+  numBytes = avpicture_get_size(AV_PIX_FMT_RGB24, pCodecCtx->width, pCodecCtx->height);
+  uint8_t *buffer = (uint8_t *) av_malloc(numBytes * sizeof(uint8_t));
+
+  avpicture_fill((AVPicture *) pFrame, buffer, AV_PIX_FMT_RGB24, pCodecCtx->width, pCodecCtx->height);
+
+  // read the frame
+  AVPacket packet;
+
+  int framesNumber = 0;
+  while (av_read_frame(pFormatCtx, &packet) >= 0)
+    {
+      if(packet.stream_index != 0)
+	continue;
+
+      int ret = avcodec_decode_video2(pCodecCtx, pFrame, &frameFinished, &packet);
+      if (ret > 0)
+        {
+	  printf("Frame is decoded, size %d\n", ret);
+	  pFrame->quality = 4;
+	  return pFrame;
+        }
+      else
+	printf("Error [%d] while decoding frame: %s\n", ret, strerror(AVERROR(ret)));
+    }
+}
+
+// main entry point
+int main(int argc, char** argv) 
+{
+  // the bouncer application should take a single command line argument.
+  if (argc != 2) {
+    printf("Illegal Argument Amount\n");
+    printf("usage: bouncer filename.jpg\n");
+    return 1;
+  }
+
+  // application should reject non-jpg files (by simply testing the extension).  
+  char * filename = argv[1];
+  char * ext = strrchr(filename, '.');
+  if (ext) {
+    if (strcmp(ext + 1, "jpg") != 0) {
+      printf("Illegal File Extension\n");
+      return 1;
+    }
+  }
+  else {
+    printf("Illegal File Extension\n");
+  }
+
+}
+
+
--- bouncer.cpp
+++ bouncer.cpp
@@ -7,56 +7,61 @@
 //
 
 #include "bouncer.h"
-#include "iostream"
-#include "string"
-#include "frame.h"
-#include "avformat.h"
-#include "avcodec.h"
-#include "pixfmt.h"
+#include <iostream>
+#include <string>
+
 using namespace std;
 
 int main (int argc, char* argv[]) {
-    //    The bouncer application should take a single command line argument.
+    // The bouncer application should take a single command line argument.
     if ( argc != 2 ) {
         cout << "Invalid Argument Amount" << endl;
         cout << "usage: bouncer filename.jpg" << endl;
     }
     
-    //This argument specifies the name of the background image file.  We will guarantee that the input file is always a .jpg file, and your application should reject non-jpg files (by simply testing the extension).
+    // This argument specifies the name of the background image file.  We will guarantee that the input file is always a .jpg file, and your application should reject non-jpg file
+    // (by simply testing the extension).
     string filename = argv[1];
     if (filename.substr(filename.find_last_of(".") + 1) != "jpg") {
         cout << "Invalid File Extension" << endl;
     }
+
+    OpenImage(filename);
     
-    //    When drawn on the background image, the sphere(s) should occupy a 'reasonable' percentage of the image space (perhaps 5-20%), and should clearly bounce at a 'reasonable' rate.  The goal should simply be to allow the user to still see most of the background image, but also see detail in the sphere shading.
-    
-    
+    return 0;
 }
 
 
 extern "C" {
-    
-    
+#include <stdio.h>
+#include "libavutil/frame.h"
+#include "libavutil/pixfmt.h"
+#include "libavformat/avformat.h"
+#include "libavcodec/avcodec.h"
+
     AVFrame* OpenImage(const char* imageFileName)
     {
+      av_register_all();
         AVFormatContext *pFormatCtx;
         
-        if(av_open_input_file(&pFormatCtx, imageFileName, NULL, 0, NULL)!=0)
+	// open the JPG file.
+        if(av_open_input_file(&pFormatCtx, filename, NULL, 0, NULL) != 0)
         {
-            printf("Can't open image file '%s'\n", imageFileName);
+            printf("Can't open image file '%s'\n", filename);
             return NULL;
         }
         
-        dump_format(pFormatCtx, 0, imageFileName, false);
+	// dump file header info to std error
+        av_dump_format(pFormatCtx, 0, imageFileName, false);
         
         AVCodecContext *pCodecCtx;
         
         pCodecCtx = pFormatCtx->streams[0]->codec;
         pCodecCtx->width = W_VIDEO;
         pCodecCtx->height = H_VIDEO;
-        pCodecCtx->pix_fmt = PIX_FMT_YUV420P;
+        pCodecCtx->pix_fmt = AV_PIX_FMT_RGB8;
         
-        // Find the decoder for the video stream
+        // find the decoder for the stream
         AVCodec *pCodec = avcodec_find_decoder(pCodecCtx->codec_id);
         if (!pCodec)
         {
@@ -64,16 +69,15 @@ extern "C" {
             return NULL;
         }
         
-        // Open codec
-        if(avcodec_open(pCodecCtx, pCodec)<0)
+        // open codec
+        if(avcodec_open(pCodecCtx, pCodec) < 0)
         {
             printf("Could not open codec\n");
             return NULL;
         }
         
-        //
-        AVFrame *pFrame;
-        
+       
+        AVFrame *pFrame; 
         pFrame = avcodec_alloc_frame();
         
         if (!pFrame)
@@ -83,16 +87,15 @@ extern "C" {
         }
         
         int frameFinished;
-        int numBytes;
+        int n_bytes;
         
-        // Determine required buffer size and allocate buffer
-        numBytes = avpicture_get_size(PIX_FMT_YUVJ420P, pCodecCtx->width, pCodecCtx->height);
+        // determine required buffer size and allocate buffer
+        n_bytes = avpicture_get_size(AV_PIX_FMT_RGB8, pCodecCtx->width, pCodecCtx->height);
         uint8_t *buffer = (uint8_t *) av_malloc(numBytes * sizeof(uint8_t));
         
-        avpicture_fill((AVPicture *) pFrame, buffer, PIX_FMT_YUVJ420P, pCodecCtx->width, pCodecCtx->height);
+        avpicture_fill((AVPicture *) pFrame, buffer, AV_PIX_FMT_RGB8, pCodecCtx->width, pCodecCtx->height);
         
-        // Read frame
-        
+        // read the frame
         AVPacket packet;
         
         int framesNumber = 0;
